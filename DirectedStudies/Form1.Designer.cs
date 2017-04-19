2d69ce07e74edf9d06034aad1f7f96832131ecde
ja04279 <ja04279@CEIT2552212X032>
2017-04-19 12:27:06 -0400

    Add project files.

---
+++ DirectedStudies/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace DirectedStudies
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDownX = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownY = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownT = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownT)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(349, 127);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDownX
+            // 
+            this.numericUpDownX.DecimalPlaces = 10;
+            this.numericUpDownX.Location = new System.Drawing.Point(163, 290);
+            this.numericUpDownX.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.numericUpDownX.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDownX.Name = "numericUpDownX";
+            this.numericUpDownX.Size = new System.Drawing.Size(119, 22);
+            this.numericUpDownX.TabIndex = 1;
+            // 
+            // numericUpDownY
+            // 
+            this.numericUpDownY.DecimalPlaces = 10;
+            this.numericUpDownY.Location = new System.Drawing.Point(388, 290);
+            this.numericUpDownY.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.numericUpDownY.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDownY.Name = "numericUpDownY";
+            this.numericUpDownY.Size = new System.Drawing.Size(119, 22);
+            this.numericUpDownY.TabIndex = 2;
+            // 
+            // numericUpDownT
+            // 
+            this.numericUpDownT.DecimalPlaces = 10;
+            this.numericUpDownT.Location = new System.Drawing.Point(603, 290);
+            this.numericUpDownT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.numericUpDownT.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDownT.Name = "numericUpDownT";
+            this.numericUpDownT.Size = new System.Drawing.Size(119, 22);
+            this.numericUpDownT.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 290);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "X:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(337, 291);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Y:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(552, 291);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "T:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(385, 246);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Manual Input";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(141, 324);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(568, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(727, 428);
+            this.btnCalc.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(154, 35);
+            this.btnCalc.TabIndex = 9;
+            this.btnCalc.Text = "Calcualte";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 470);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDownT);
+            this.Controls.Add(this.numericUpDownY);
+            this.Controls.Add(this.numericUpDownX);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownT)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDownX;
+        private System.Windows.Forms.NumericUpDown numericUpDownY;
+        private System.Windows.Forms.NumericUpDown numericUpDownT;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCalc;
+    }
+}
+
---
+++ DirectedStudies/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DirectedStudies
+{
+    public partial class Form1 : Form
+    {
+        double[][][] input = new double[365][][];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                Stream stream = openFileDialog1.OpenFile();
+
+                StreamReader reader = new StreamReader(stream);
+                String line;
+
+                //store the times
+                Dictionary<double, int?> times = new Dictionary<double, int?>();
+
+                double[] arr = new double[4];
+
+                //store each line
+                List<double[]> lines = new List<double[]>();
+
+                //while loop to go through each line and count the number of arrays for the 2 level
+                while ((line = reader.ReadLine()) != null)
+                {
+                    //LINQ to break up string into int array by '\t'
+                    arr = line.Split('\t').Select(n => Convert.ToDouble(n)).ToArray();
+                   
+                    if (times[arr[2]] == null)
+                    {
+                        times[arr[2]] = 1;
+                    } else
+                    {
+                        times[arr[2]]++;
+                    }
+                }
+
+               //reader.
+                //go through and start putting the data into the array
+                for (int i = 0; i < 365; i++)
+                {
+                    input[i] = new double[(int)times[i]][];
+
+                    //while loop to go through each line
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        //LINQ to break up string into int array by '\t'
+                        arr = line.Split('\t').Select(n => Convert.ToDouble(n)).ToArray();
+
+                        if (times[arr[2]] == null)
+                        {
+                            times[arr[2]] = 1;
+                        }
+                        else
+                        {
+                            times[arr[2]]++;
+                        }
+                    }
+                }
+
+                foreach (KeyValuePair<double, int?> entry in times)
+                {
+                    input[(int)arr[2]] = new double[times.Count][];
+                }
+
+                reader.Close();
+
+                //pass the input to the object to calculate
+            }
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            double fieldX, fieldY, fieldT;
+
+            fieldX = (double) numericUpDownX.Value;
+            fieldY = (double) numericUpDownY.Value;
+            fieldT = (double) numericUpDownT.Value;
+
+            //pass to the object to calculate
+           // IDW(fieldX, fieldY, fieldT, input);
+        }
+
+        //----- Alex's Code begins -----//
+        /* The inputs are the:
+         * fieldX = the X value inputted on the interface
+         * fieldY = the Y value inputted on the interface
+         * fileX = all the X values in the list from a certain time period
+         * fileY = all the Y values in the list from a certain time period
+         * fileC = all the C (weighted values) in a list from a certain time period
+         */
+       /* public double IDW(double fieldX, double fieldY, double fieldT, Dictionary<double, Dictionary<char, decimal>[]> input)
+        {
+        
+            //decimal x = input[12.0][0]['X'];
+            double total = 0; //returned variable
+            for(int i=0; i < fileX.Length; i++)
+            {
+                //The distance fomula is Square Root((x2 - x1)^2 - (y2 -y1)^2)
+                //distY is the x2 - x1, distY is the y2 - y1
+                double distX = fileX[i] - fieldX; double distY = fieldY - fileY[i];
+                //in IDW, the weight loses strength over distance, so: (weighted value)/ (distance) 
+                total += fileC[i]/(Math.Sqrt((distX * distX) - (distY * distY)));
+            }
+            //if we assume that beta in the IDW formula is 1 (giving the weighting a linear distribution),
+            //then we can simply divide the total by how many objects are being calculated (a basic average)
+            total /= fileX.Length;
+
+            //since the value returned to the screen is a float, and Math.Sqrt outputs as a double, the value has
+            //to be recast as a float when returning.
+            return (float)total;
+        }*/
+        //----- Alex's Code Ends -----//
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
